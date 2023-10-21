07be175fe3d6fdb4f0820160b052edc5c34c009b
ASahrlingHamm <anna.sahrling-hamm@edu.edugrade.se>
2023-10-21 11:33:40 +0200

    Senaste versionen

--- src/main/java/com/asahrlinghamm/mediaservice/Services/MediaService.java
+++ src/main/java/com/asahrlinghamm/mediaservice/Services/MediaService.java
@@ -17,26 +17,36 @@ public class MediaService implements MediaServiceInterface{
 
 
     @Override
-    public Media addSong(Media media) {
+    public Media addMedia(Media media) {
         return mediaRepository.save(media);
     }
 
     @Override
-    public List<Media> getAllSongs() {
-        return mediaRepository.findMediaByMediaTypeContains("song");
+    public Media updateMedia(Media media, ObjectId id) {
+        Media m = mediaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Media", "Id", id));
+        m.setTitle(media.getTitle());
+        m.setMediaType(media.getMediaType());
+        m.setUrl(media.getUrl());
+        m.setReleaseDate(media.getReleaseDate());
+        m.setGenres(media.getGenres());
+        m.setArtists(media.getArtists());
+        mediaRepository.save(m);
+        return m;
     }
 
     @Override
-    public Media updateSong(Media song, ObjectId id) {
-        Media s = mediaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Song", "Id", id));
-        s.setTitle(song.getTitle());
-        s.setMediaType(song.getMediaType());
-        s.setUrl(song.getUrl());
-        s.setReleaseDate(song.getReleaseDate());
-        s.setGenres(song.getGenres());
-        s.setArtists(song.getArtists());
-        mediaRepository.save(s);
-        return s;
+    public void deleteMedia(ObjectId id) {
+        mediaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Media", "Id", id));
+        mediaRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Media> getAllMedia() {
+        return mediaRepository.findAll();
+    }
+
+    public List<Media> getAllMediaByMediaType(String mediaType) {
+        return mediaRepository.findMediaByMediaTypeContains(mediaType);
     }
 
     public List<Media> getMediaByGenre(String genre){
@@ -55,74 +65,19 @@ public class MediaService implements MediaServiceInterface{
         return mediaRepository.findMediaByArtistsContainsIgnoreCase(artists);
     }
 
-    @Override
-    public void deleteSong(ObjectId id) {
-        mediaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Music", "Id", id));
-        mediaRepository.deleteById(id);
+
+/*    @Override
+    public List<Media> getAllMediaByArtist(Media artists) {
+
+        return mediaRepository.findMediaByArtistsContaining();
     }
 
     @Override
-    public Media addPod(Media pod) {
-        return mediaRepository.save(pod);
-    }
+    public List<Media> getAllMediaByGenre(Media genres) {
 
-    @Override
-    public List<Media> getAllPods() {
-        return mediaRepository.findMediaByMediaTypeContains("podcast");
-    }
+        return mediaRepository.findMediaByGenresContaining();
+    }*/
 
-    @Override
-    public Media updatePod(Media pod, ObjectId id) {
-        Media p = mediaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Pod", "Id", id));
-        p.setTitle(pod.getTitle());
-        p.setMediaType(pod.getMediaType());
-        p.setUrl(pod.getUrl());
-        p.setReleaseDate(pod.getReleaseDate());
-        p.setGenres(pod.getGenres());
-        p.setArtists(pod.getArtists());
-        mediaRepository.save(p);
-        return p;
-    }
-
-    @Override
-    public void deletePod(ObjectId id) {
-        mediaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Pod", "Id", id));
-        mediaRepository.deleteById(id);
-    }
-
-    @Override
-    public Media addVideo(Media video) {
-        return mediaRepository.save(video);
-    }
-
-    @Override
-    public List<Media> getAllVideos() {
-        return mediaRepository.findMediaByMediaTypeContains("movie");
-    }
-
-    @Override
-    public Media updateVideo(Media video, ObjectId id) {
-        Media v = mediaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Video", "Id", id));
-        v.setTitle(video.getTitle());
-        v.setMediaType(video.getMediaType());
-        v.setUrl(video.getUrl());
-        v.setReleaseDate(video.getReleaseDate());
-        v.setGenres(video.getGenres());
-        v.setArtists(video.getArtists());
-        mediaRepository.save(v);
-        return v;
-    }
-
-    @Override
-    public void deleteVideo(ObjectId id) {
-        mediaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Video", "Id", id));
-        mediaRepository.deleteById(id);
-    }
-
-    @Override
-    public List<Media> getAllMedia() {
-        return mediaRepository.findAll();
-    }
 
 
 }
