78f5209d954941bad5f9d96d73f879061639c510
AndrHalv <andreas.halvardson@edu.edugrade.se>
2023-10-24 10:00:56 +0200

    Added DiscoveryClient

--- src/main/java/com/asahrlinghamm/mediaservice/MediaserviceApplication.java
+++ src/main/java/com/asahrlinghamm/mediaservice/MediaserviceApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableMongoRepositories
+@EnableDiscoveryClient
 public class MediaserviceApplication {
 
 	public static void main(String[] args) {
